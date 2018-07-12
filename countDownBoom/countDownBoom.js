857e56fe10e334defb26482dfa8c37e227a93d6c
ak.padmalayan <adityakiran.padmalayan@gmail.com>
2018-07-13 00:22:44 +0530

    count down boom done

--- countDownBoom/countDownBoom.js
+++ countDownBoom/countDownBoom.js
@@ -1,10 +1,29 @@
 (function () {
     const countDownBoom = angular.module("countDownBoom", []);
 
-    function boomController($scope) {
+    function boomController($scope, $interval, $log) {
+        let countDownInterval = null;
+        function decrementCounter(){
+            $scope.timerCount --;
+            $scope.boom = ($scope.timerCount === 0);
+        }
+
+        $scope.saveTheWorld = function() {
+            $scope.saved = true;
+            if(countDownInterval)
+            {
+                $log.info("count down stopped");
+                $interval.cancel(countDownInterval);
+            }
+        }
+
+        $scope.timerCount = 10;
         $scope.heading = "!! ATTENTION !!";
+        $scope.boom = false;
+        $scope.saved = false;
+        countDownInterval = $interval(decrementCounter, 1000, 10)
     }
 
-    countDownBoom.controller("boomController", ["$scope", boomController])
+    countDownBoom.controller("boomController", ["$scope", "$interval", "$log",boomController])
 
 }());
